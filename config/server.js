fe8392923161c7e7254f8ac64475d4e3796c4864
Hector Franco <hector.francomasmela@davivienda.com>
2022-11-21 09:15:06 -0500

    CORS

--- config/server.js
+++ config/server.js
@@ -55,16 +55,16 @@ class Server {
                 'https://hosting-day-2-day.web.app'
             ],
         }
-        // this.app.use(cors(corsOptions));
+        this.app.use(cors(corsOptions));
 
 
-        this.app.use(function (req, res, next) {
-            res.header("Access-Control-Allow-Origin", '*');
-            res.header("Access-Control-Allow-Credentials", true);
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-            res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-            next();
-        });
+        // this.app.use(function (req, res, next) {
+        //     res.header("Access-Control-Allow-Origin", '*');
+        //     res.header("Access-Control-Allow-Credentials", true);
+        //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        //     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+        //     next();
+        // });
 
         // Lectura y parseo del body
         this.app.use(express.json({ limit: '100mb' }));
