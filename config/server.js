cde32a4f772ee5c6d85b2e1ce83c1e7bef4924d3
Hector Franco <hector.francomasmela@davivienda.com>
2022-11-21 09:10:18 -0500

    CORS

--- config/server.js
+++ config/server.js
@@ -16,17 +16,17 @@ class Server {
 
         // Rutas a los diferentes Endpoints.
         this.paths = {
-            auth:       '/api/auth',
-            reports:    '/api/reports',
-            users:      '/api/users',
-            activities: '/api/activities', 
-            categories: '/api/categories', 
-            countries:  '/api/countries', 
-            companies:  '/api/companies',
-            roles:      '/api/roles',
-            areas:      '/api/areas',
-            calendars:  '/api/calendars',
-            celulas:    '/api/celulas',
+            auth: '/api/auth',
+            reports: '/api/reports',
+            users: '/api/users',
+            activities: '/api/activities',
+            categories: '/api/categories',
+            countries: '/api/countries',
+            companies: '/api/companies',
+            roles: '/api/roles',
+            areas: '/api/areas',
+            calendars: '/api/calendars',
+            celulas: '/api/celulas',
         };
 
         // Conectar a base de datos
@@ -43,7 +43,7 @@ class Server {
         await dbConnection();
     }
 
-    
+
 
     middlewares() {
         // CORS
@@ -55,24 +55,33 @@ class Server {
                 'https://hosting-day-2-day.web.app'
             ],
         }
-        this.app.use(cors({origin: '*'}));
+        // this.app.use(cors(corsOptions));
+
+
+        app.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", '*');
+            res.header("Access-Control-Allow-Credentials", true);
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+            res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+            next();
+        });
 
         // Lectura y parseo del body
         this.app.use(express.json({ limit: '100mb' }));
     }
 
     routes() {
-        this.app.use(this.paths.users,      require('../routes/users'));
-        this.app.use(this.paths.reports,    require('../routes/reports'));
-        this.app.use(this.paths.auth,       require('../routes/auth'));
+        this.app.use(this.paths.users, require('../routes/users'));
+        this.app.use(this.paths.reports, require('../routes/reports'));
+        this.app.use(this.paths.auth, require('../routes/auth'));
         this.app.use(this.paths.activities, require('../routes/activities'));
         this.app.use(this.paths.categories, require('../routes/categories.routing'));
-        this.app.use(this.paths.countries,  require('../routes/countries.routing'));
-        this.app.use(this.paths.companies,  require('../routes/companies.routing'));
-        this.app.use(this.paths.roles,      require('../routes/roles.routing'));
-        this.app.use(this.paths.areas,      require('../routes/areas.routing'));
-        this.app.use(this.paths.calendars,  require('../routes/calendars.routing'));
-        this.app.use(this.paths.celulas,    require('../routes/celulas.routing'));
+        this.app.use(this.paths.countries, require('../routes/countries.routing'));
+        this.app.use(this.paths.companies, require('../routes/companies.routing'));
+        this.app.use(this.paths.roles, require('../routes/roles.routing'));
+        this.app.use(this.paths.areas, require('../routes/areas.routing'));
+        this.app.use(this.paths.calendars, require('../routes/calendars.routing'));
+        this.app.use(this.paths.celulas, require('../routes/celulas.routing'));
     }
 
     listen() {
